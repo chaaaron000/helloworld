ece5278cac802229cba3f519564286fe17df817d
chaaaron000 <chaaaron000@DESKTOP-NCB3BUN.localdomain>
2020-07-01 22:02:59 +0900

    complete background jpg

---
+++ background.js
@@ -0,0 +1,22 @@
+const body = document.querySelector("body");
+
+const IMG_NUMBER = 28;
+
+function paintImange(imgNumber) {
+  const image = new Image();
+  image.src = `images/${imgNumber + 1}.jpg`; // png는 어떻게 구분해서 넣어야 할까 씨부레!
+  image.classList.add("bgImage");
+  body.appendChild(image);
+}
+
+function genRandom() {
+  const number = Math.floor(Math.random() * IMG_NUMBER);
+  return number;
+}
+
+function init() {
+  const randomNumber = genRandom();
+  paintImange(randomNumber);
+}
+
+init();
