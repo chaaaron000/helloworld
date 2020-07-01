8bece3ba1163668aba8c854f903ef9e3608f370d
chaaaron000 <chaaaron000@DESKTOP-NCB3BUN.localdomain>
2020-07-01 22:28:40 +0900

    complete background jpg png

--- background.js
+++ background.js
@@ -7,6 +7,12 @@ function paintImange(imgNumber) {
   image.src = `images/${imgNumber + 1}.jpg`; // png는 어떻게 구분해서 넣어야 할까 씨부레!
   image.classList.add("bgImage");
   body.appendChild(image);
+
+  image.onerror = function () {
+    image.src = `images/${imgNumber + 1}.png`;
+  };
+  image.classList.add("bgImage");
+  body.appendChild(image);
 }
 
 function genRandom() {
